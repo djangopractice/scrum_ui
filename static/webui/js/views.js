e16ccf522e3f412fdf8901e0e11619be4c582b0a
Dave Koopman <dkoopman@godaddy.com>
2014-12-27 12:06:39 -0700

    Fixed cancel button.  Started SprintView

--- static/webui/js/views.js
+++ static/webui/js/views.js
@@ -85,6 +85,27 @@
         }
     });
 
+    var SprintView = TemplateView.extend({
+        templateName: '#sprint-template',
+        initialize: function (options) {
+            var self = this;
+            TemplateView.prototype.initialize.apply(this, arguments);
+            this.sprintId = options.sprintId;
+            this.sprint = null;
+            app.collections.ready.done(function () {
+                self.sprint = app.sprints.push({id: self.sprintId});
+                self.sprint.fetch({
+                    success: function () {
+                        self.render();
+                    }
+                });
+            });
+        },
+        getContext: function () {
+            return {sprint: this.sprint};
+        }
+    });
+
     var HomepageView = TemplateView.extend({
         templateName: '#home-template',
         events: {
@@ -154,5 +175,6 @@
     app.views.HomepageView = HomepageView;
     app.views.LoginView = LoginView;
     app.views.HeaderView = HeaderView;
+    app.views.SprintView = SprintView;
 
 })(jQuery, Backbone, _, app);
